6148f76bb40837edc3e48f973b6ed1d334885e35
Hillarysidney <hillarysidney4@gmail.com>
2023-10-05 14:46:16 +0000

    file update

---
+++ models/state.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" State Module for HBNB project """
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, Integer, DateTime, ForeignKey
+from sqlalchemy.orm import relationship
+from models.city import City
+import os
+
+class State(BaseModel, Base):
+    """ State class """
+    __tablename__ = "states"
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", backref="state",
+                          cascade="all, delete-orphan")
+
+    @property
+    def cities(self):
+        """getter attribute cities that returns the list of City"""
+        from models import storage
+        new_list = []
+        new_cities = storage.all(City).values()
+        for city in new_cities:
+            if self.id == city.state_id:
+                new_list.append(city)
+        return new_list
