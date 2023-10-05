d81cfc60dfa9927c9515b4fdbf12c81b53812d99
Hillarysidney <hillarysidney4@gmail.com>
2023-10-05 15:41:25 +0000

    file updated

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,32 +1,25 @@
 #!/usr/bin/python3
-"""Starts a Flask web application.
-
-The application listens on 0.0.0.0, port 5000.
-Routes:
-    /states_list: HTML page with a list of all State objects in DBStorage.
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug  1 11:15:54 2023
+@author: Obi Nancy
 """
 from models import storage
-from flask import Flask
-from flask import render_template
+from models.state import State
+from flask import Flask, render_template
+my_app = Flask(__name__)
 
-app = Flask(__name__)
-
-
-@app.route("/states_list", strict_slashes=False)
-def states_list():
-    """Displays an HTML page with a list of all State objects in DBStorage.
-
-    States are sorted by name.
+@my_app.teardown_appcontext
+def appcontext_teardown(self):
+    """use storage for fetching data from the storage engine
     """
-    states = storage.all("State")
-    return render_template("7-states_list.html", states=states)
-
-
-@app.teardown_appcontext
-def teardown(exc):
-    """Remove the current SQLAlchemy session."""
     storage.close()
 
+@my_app.route('/states_list', strict_slashes=False)
+def state_info():
+    """Display a HTML page inside the tag BODY"""
+    return render_template('7-states_list.html',
+                           states=storage.all(State))
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+if __name__ == '__main__':
+    my_app.run(host='0.0.0.0', port=5000)
